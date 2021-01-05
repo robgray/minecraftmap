04db26c4018a3c43c6bd70747119048c7d139607
Rob Gray <rob.gray@outlook.com>
2021-01-06 09:18:00 +1000

    Fix main page layout

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import { initializeIcons, Stack } from '@fluentui/react';
 import { realm } from './api/testData';
 import Sidebar from "./components/Sidebar";
+import { DefaultPalette, Text,  } from 'office-ui-fabric-react'
 
 function App() {
   initializeIcons();
@@ -12,19 +13,26 @@ function App() {
   const [realmData, setRealmData] = useState(realm);
   const sidebarItemStyles = {
     root: {
-      width: "200px"
+      padding: 5,
+      width: 320
     }
   }
-  
+
+  const mapStackItemStyles = {
+    root: {
+      display: "flex",
+      paddingLeft: 5,
+      width: "100%"
+    }
+  }
+
   return (
     <Stack horizontal>
-      <Stack.Item styles={sidebarItemStyles}>
+      <Stack.Item styles={sidebarItemStyles} align="start">
         <Sidebar data={realmData}/>
       </Stack.Item>
-      <Stack.Item>
-        <div id="map">
-          <Map />
-        </div>
+      <Stack.Item styles={mapStackItemStyles} align="end">
+        <Map data={realmData}  />
       </Stack.Item>
     </Stack>
   );
--- front/src/components/MinecraftMap.tsx
+++ front/src/components/MinecraftMap.tsx
@@ -1,44 +1,50 @@
-import { useContext }  from 'react';
+import { useContext, useState }  from 'react';
 import { MapContainer, Marker, Popup, LayerGroup } from 'react-leaflet';
-import { CRS, LatLngBounds, LatLngTuple, LatLng } from 'leaflet';
+import { CRS, LatLngBounds, LatLngTuple } from 'leaflet';
 import { LayerContext } from './context/LayerContext';
+import { IRealm } from '../api/realm';
 
-const pretendLocations: LatLngTuple[] = [[100,80], [250,7], [-374,902], [-150,-900]];
-const bounds = getBoundsFromLocations(pretendLocations);
-const zoom: number = -10;
-const center: LatLng = bounds.getCenter();
+interface IMinecraftMapProps {
+    data: IRealm
+}
 
-const MinecraftMap:React.FC = () => {
+const MinecraftMap: React.FC<IMinecraftMapProps> = (props: IMinecraftMapProps) => {
+
+    const getBoundsFromLocations = (locs: LatLngTuple[]):LatLngBounds =>
+    {
+        let smallestX = Number.MAX_SAFE_INTEGER;
+        let smallestY = Number.MAX_SAFE_INTEGER;
+        let largestX = Number.MIN_SAFE_INTEGER;
+        let largestY = Number.MIN_SAFE_INTEGER;
+
+        locs.forEach((point) => {
+            if (point[0] < smallestX) smallestX = point[0];
+            if (point[0] > largestX) largestX = point[0];
+
+            if (point[1] < smallestY) smallestY = point[1];
+            if (point[1] > largestY) largestY = point[1];
+        });
+        return new LatLngBounds([smallestX, smallestY], [largestX, largestY]);
+    }
+
     const { point } = useContext(LayerContext);
+    const [ locations, setLocations] = useState((): LatLngTuple[] => props.data.locations.map(location => [ location.coordinate.X, location.coordinate.Y]));
+    const [ bounds, setBounds] = useState(() => getBoundsFromLocations(locations))
+    const [ zoomLevel, setZoomLevel] = useState(-10);
+    const [ center, setCenter] = useState(bounds.getCenter());
 
     return (
-        <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} crs={CRS.Simple} bounds={bounds}>
+        <MapContainer center={center} zoom={zoomLevel} scrollWheelZoom={true} crs={CRS.Simple} bounds={bounds}>
             <LayerGroup>
                 {point}
             </LayerGroup>
-            {pretendLocations.map(location => (
-                <Marker position={location}>
+            {props.data.locations.map(location => (
+                <Marker position={[location.coordinate.X, location.coordinate.Y]}>
+                    <Popup>{location.name}</Popup>
                 </Marker>
             ))}
         </MapContainer>
     )
-}
-
-function getBoundsFromLocations(locations: LatLngTuple[]):LatLngBounds
-{
-    let smallestX = Number.MAX_SAFE_INTEGER;
-    let smallestY = Number.MAX_SAFE_INTEGER;
-    let largestX = Number.MIN_SAFE_INTEGER;
-    let largestY = Number.MIN_SAFE_INTEGER;
-
-    locations.forEach((point) => {
-        if (point[0] < smallestX) smallestX = point[0];
-        if (point[0] > largestX) largestX = point[0];
-
-        if (point[1] < smallestY) smallestY = point[1];
-        if (point[1] > largestY) largestY = point[1];
-    });
-    return new LatLngBounds([smallestX, smallestY], [largestX, largestY]);
 }
 
 export default MinecraftMap;
